f9089c677fd10e168ece456dae12cbe8caf985af
André Vennberg <andre.vennberg@gmail.com>
2017-11-14 17:16:57 +0100

    Added Super Monkey.

---
+++ Assets/Scripts/Fire Functions/TowerSuper_Fire.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSuper_Fire : FireFunction {
+    private TowerController tower;
+
+    public override void Initialize(GameObject obj) {
+        canFire = true;
+        tower = obj.GetComponent<TowerController>();
+    }
+
+    public override IEnumerator Fire(Enemy target) {
+        Projectile dart = Instantiate(
+            tower.projectile.gameObject, tower.transform.position, tower.transform.rotation
+        ).GetComponent<Projectile>();
+
+        dart.Durability = tower.projectileDurability;
+
+        Vector3 vectorToTarget = target.transform.position - dart.transform.position;
+        dart.GetComponent<Rigidbody2D>().velocity = vectorToTarget * dart.speed;
+
+        yield return new WaitForSeconds(tower.fireDelay);
+
+        canFire = true;
+    }
+}
---
+++ Assets/Scripts/Upgrades/TowerSuper_EpicRange.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSuper_EpicRange : Upgrade {
+    public float newAttackRange;
+
+    public override void AddUpgrade(GameObject obj) {
+        TowerController tower = obj.GetComponent<TowerController>();
+        tower.AttackRange = newAttackRange;
+    }
+}
