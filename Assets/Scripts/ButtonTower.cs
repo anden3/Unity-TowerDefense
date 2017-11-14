fecb9127435f0844d9bd91d70b55ecc4b36831d2
André Vennberg <andre.vennberg@gmail.com>
2017-11-14 13:05:44 +0100

    Changed FireFunctions folder to Fire Functions.

--- Assets/Scripts/ButtonTower.cs
+++ Assets/Scripts/ButtonTower.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonTower : MonoBehaviour {
-    public GameObject tower;
+    public TowerController tower;
 
     private Button button;
+    private Text towerCostField;
 
 	private void Awake() {
         button = gameObject.GetComponent<Button>();
         button.onClick.AddListener(ButtonClicked);
+
+        towerCostField = transform.GetChild(0).GetComponent<Text>();
+        towerCostField.text = tower.cost.ToString();
     }
 
     private void ButtonClicked() {
-        int towerCost = tower.GetComponent<TowerController>().cost;
+        int towerCost = tower.cost;
 
         if (towerCost > GameController.instance.Money) {
             return;
         }
 
-        Instantiate(tower, Input.mousePosition, Quaternion.identity);
+        Instantiate(tower.gameObject, Input.mousePosition, Quaternion.identity);
     }
 }
